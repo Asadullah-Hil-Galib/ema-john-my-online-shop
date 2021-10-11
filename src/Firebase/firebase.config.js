808368e3f94a37feeea73031de53609fd5c695d5
Adadulla Hil Galib <coderboygalif@gmail.com>
2021-10-11 20:08:10 +0600

    comited firebase login logout system add in ema john

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCaLS3mkccjyGvIBIlYDMwfez3tfLfz1JM",
+    authDomain: "ema-john-shop-61b29.firebaseapp.com",
+    projectId: "ema-john-shop-61b29",
+    storageBucket: "ema-john-shop-61b29.appspot.com",
+    messagingSenderId: "749750438237",
+    appId: "1:749750438237:web:19414400e17f89205940b4"
+  };
+export default firebaseConfig;
